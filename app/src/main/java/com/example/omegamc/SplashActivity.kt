0bfd95ad0e0fe7fed86437707ae6e3dca87ffe57
romeoj988 <romeoj988@gmail.com>
2026-04-23 01:15:04 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/example/omegamc/SplashActivity.kt
+++ app/src/main/java/com/example/omegamc/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
+
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
